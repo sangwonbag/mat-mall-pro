b6fa2d1ab3506ff6025f8b7d77ec55b66b6a9ad7
Wix Remote Machine <support@wix.com>
2025-11-10 07:02:43 +0000

    검색결과 8열로 변경 중

--- src/components/pages/SearchPage.tsx
+++ src/components/pages/SearchPage.tsx
@@ -207,7 +207,7 @@ export default function SearchPage() {
                 </h2>
               </div>
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 gap-6">
                 {filteredProducts.map((product) => (
                   <motion.div
                     key={product._id}
