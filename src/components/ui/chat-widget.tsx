cd72dadf1ccf7034460a4d95d5d2fdd35f1c9294
Wix Remote Machine <support@wix.com>
2025-11-13 23:09:18 +0000

    상담문의 창 위치 위로 이동 중

--- src/components/ui/chat-widget.tsx
+++ src/components/ui/chat-widget.tsx
@@ -185,7 +185,7 @@ export function ChatWidget({ className = '' }: ChatWidgetProps) {
   };
 
   return (
-    <div className={`fixed bottom-6 right-6 z-50 ${className}`}>
+    <div className={`fixed bottom-24 right-6 z-50 ${className}`}>
       {/* Chat Button */}
       {!isOpen && (
         <Button
