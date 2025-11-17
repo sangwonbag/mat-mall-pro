527c32a04c8061e2b7dc643df22be88012cfe350
Wix Remote Machine <support@wix.com>
2025-11-17 06:13:06 +0000

    admin login 페이지 삭제 중

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -8,7 +8,7 @@ import ProductDetailPage from '@/components/pages/ProductDetailPage';
 import QuotePage from '@/components/pages/QuotePage';
 import AdminPage from '@/components/pages/AdminPage';
 import ProfilePage from '@/components/pages/ProfilePage';
-import AdminLoginPage from '@/components/pages/AdminLoginPage';
+
 import AdminDashboardPage from '@/components/pages/AdminDashboardPage';
 import AdminPdfPage from '@/components/pages/AdminPdfPage';
 import CatalogTrendyPage from '@/components/pages/CatalogTrendyPage';
@@ -54,10 +54,7 @@ const router = createBrowserRouter([
         path: "admin",
         element: <AdminPage />,
       },
-      {
-        path: "admin/login",
-        element: <AdminLoginPage />,
-      },
+
       {
         path: "admin/dashboard",
         element: <AdminDashboardPage />,
